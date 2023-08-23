1122cdce3db9a2fb193141505584d41fc8a35226
ariessalvador <142958841+ariessalvador@users.noreply.github.com>
2023-08-23 11:23:21 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,1 +1,7 @@
-
+public class Hello{
+  public static void main(String args[]){
+    System.out.println("Hello, Software Engineering Course!");
+    System.out.println("My name is Salvador, John Aries");
+  }
+}
+    
