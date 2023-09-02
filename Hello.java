fbfd376011a4fe072b760465753e54c0633cc0cd
Aries Salvador <johnariessalvador@gmail.com>
2023-09-02 10:48:09 +0800

    adding some changes to Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,8 @@ public class Hello{
   public static void main(String args[]){
     System.out.println("Hello, Software Engineering Course!");
     System.out.println("My name is Salvador, John Aries");
-System.out.println("That is why developers are highly-paid!");
+    System.out.println("That is why developers are highly-paid!");
+    System.out.println ("Complicated world of SoftDev that is why it pays much!");
   }
 }
     
