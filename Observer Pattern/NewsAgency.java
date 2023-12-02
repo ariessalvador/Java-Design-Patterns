84807c6395f9fcd6db70c0f1a44f59ae96240688
Aries Salvador <142958841+ariessalvador@users.noreply.github.com>
2023-12-02 21:31:18 +0800

    Update NewsAgency.java

--- Observer Pattern/NewsAgency.java
+++ Observer Pattern/NewsAgency.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class NewsAgency implements NewsAgencyObserver {
+public class NewsAgency implements NewsAgencySubject {
     private String localNews;
     private String globalNews;
     private String financialNews;
