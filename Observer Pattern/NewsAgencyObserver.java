5070a5f890261ab0f7bd310ce12788819e00f3b3
Aries Salvador <142958841+ariessalvador@users.noreply.github.com>
2023-12-02 21:31:32 +0800

    Update NewsAgencyObserver.java

--- Observer Pattern/NewsAgencyObserver.java
+++ Observer Pattern/NewsAgencyObserver.java
@@ -1,4 +1,4 @@
-public interface NewsAgencyObserver {
+public interface NewsAgencySubject {
     public void subscribe(Subscriber subscriber);
     public void unsubscribe(Subscriber subscriber);
     public void publishNews();
