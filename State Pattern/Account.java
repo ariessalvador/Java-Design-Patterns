5672337770d15caff5e7b5b540d9a5f7ed5203a9
Aries Salvador <142958841+ariessalvador@users.noreply.github.com>
2024-02-24 14:46:39 +0800

    Update Account.java
    removed the interface implementation

--- State Pattern/Account.java
+++ State Pattern/Account.java
@@ -1,14 +1,16 @@
 package StatePattern_Lab1;
 
-public class Account implements AccountState{
+public class Account {
     private AccountState accountState;
     private double balance;
     private String accountNumber;
+
     public Account(String accountNumber, double balance) {
         this.accountNumber = accountNumber;
         this.balance = balance;
         this.accountState = new ActiveState(this);
     }
+    
     public double getBalance() {
         return balance;
     }
@@ -18,22 +20,26 @@ public class Account implements AccountState{
     public void setAccountState(AccountState accountState) {
         this.accountState = accountState;
     }
-    // Methods
     public void deposit(double amount) {
         accountState.deposit(amount);
     }
+
     public void withdraw(double amount) {
         accountState.withdraw(amount);
     }
+
     public void suspend() {
         accountState.suspend();
     }
+
     public void activate() {
         accountState.activate();
     }
+
     public void close() {
         accountState.close();
     }
+
     public String toString() {
         return "Account Number: " + accountNumber + ", Balance: " + balance;
     }
